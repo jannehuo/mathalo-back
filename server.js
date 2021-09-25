56e8fcf165c2a49fd8ede4119f2b64959e4b9892
jannehuo-kesko <janne.huotari@teamit.fi>
2021-09-25 17:37:18 +0300

    Add cors

--- server.js
+++ server.js
@@ -1,47 +1,43 @@
-const express = require('express')
-const app = express()
-const portti = 1337
+const express = require('express');
+const cors = require('cors');
 
+const app = express();
+app.use(cors());
 
-
-
-
+const portti = 1337;
 
 function fibonacci(kuinkamonta) {
-  var eka = 1
-  var toka = 1
-  var kolmas = 1
-  var numerot = []
-  numerot.push(eka)
-  numerot.push(toka)
+  var eka = 1;
+  var toka = 1;
+  var kolmas = 1;
+  var numerot = [];
+  numerot.push(eka);
+  numerot.push(toka);
 
   for (let i = 0; i < kuinkamonta; i++) {
     kolmas = eka + toka;
-    numerot.push(kolmas)
+    numerot.push(kolmas);
 
     eka = toka;
     toka = kolmas;
-
   }
 
-  return numerot
+  return numerot;
 }
 
-console.log(fibonacci(50)) 
-
+console.log(fibonacci(50));
 
 app.get('/', (req, res) => {
-  res.send('skeletonni')
-})
+  res.send('skeletonni');
+});
 
 app.get('/fibonacci/:amount?', (req, res) => {
   if (req.params.amount) {
-    res.send(fibonacci(req.params.amount))
+    res.send(fibonacci(req.params.amount));
   }
-  res.send(fibonacci(15))
-
-})
+  res.send(fibonacci(15));
+});
 
 app.listen(portti, () => {
-  console.log(`servu startannu.... ${portti}`)
-})
+  console.log(`servu startannu.... ${portti}`);
+});
