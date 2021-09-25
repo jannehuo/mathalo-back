12f0320de71c0c815cae9c99de51e8489cf90950
Aki Santaniemi <santana2@aalto.fi>
2021-09-25 18:06:01 +0300

    alkulukuu

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const portti = 1337
 
 
 
-
+// #######################################
 
 function fibonacci(kuinkamonta) {
   var eka = 1
@@ -29,6 +29,45 @@ function fibonacci(kuinkamonta) {
 
 console.log(fibonacci(50)) 
 
+// #######################################
+
+var countPrimes = function(n) {
+  var arr = [1]
+
+  if(n === 1){
+return 0;
+  }
+  let count = 0;
+  for(let i = 2; i < n; i++){
+      if(i !== 2 && i % 2 == 0){
+          continue;
+      }
+
+          if(isPrime(i)){
+            arr.push(i)
+              count++;
+
+          }
+
+  }
+  return arr;
+};
+
+function isPrime(num) {
+var sqrtnum=Math.floor(Math.sqrt(num));
+  var prime = num != 1;
+  for(var i=2; i<sqrtnum+1; i++) { 
+      if(num % i == 0) {
+          prime = false;
+          break;
+      }
+  }
+  return prime;
+}
+
+console.log(countPrimes(1000))
+
+
 
 app.get('/', (req, res) => {
   res.send('skeletonni')
@@ -39,6 +78,14 @@ app.get('/fibonacci/:amount?', (req, res) => {
     res.send(fibonacci(req.params.amount))
   }
   res.send(fibonacci(15))
+
+})
+
+app.get('/prime/:until?', (req, res) => {
+  if (req.params.until) {
+    res.send(countPrimes(req.params.amount))
+  }
+  res.send(countPrimes(15))
 
 })
 
